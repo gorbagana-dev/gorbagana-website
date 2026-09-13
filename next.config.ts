7622d7bbe926b65be8177a95a29af898e9484134
Parvat Raj Singh <parvat.raj2@gmail.com>
2026-09-14 01:06:25 +0530

    chore: add Cloudflare Workers deploy pipeline (OpenNext + GitHub Actions)

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
@@ -9,3 +10,5 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+initOpenNextCloudflareForDev();
